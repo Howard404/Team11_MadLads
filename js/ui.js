54b49b4a322ff9fd98a8c2c8f1668ebec7ee4a05
rogerb5 <rbenjume@ewu.edu>
2023-04-15 08:08:27 -0700

    add filter system

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,8 @@
 /*
     This file will hold the User Interface Logic
 */
-
 const filterButtons = document.querySelectorAll('button.btn');
-
+const articleTag = Array.from(document.querySelectorAll("article.art"));
 /*
     This function loops over all
     filter buttons, and removes their active class
@@ -21,7 +20,78 @@ function changeFilterBtnColor() {
     })
 }
 
-// todo: create a filter system for buttons and genres
+/*  
+    Applies filter setting according to the 
+    button index. 
+*/
+function applyFilter() {
+    filterButtons.forEach((btn, btnIndex) => {
+        btn.addEventListener("click", () => {
+            if (btnIndex === 0) {
+                resetFilter(articleTag)
+            } else if (btnIndex > filterButtons.length) {
+                return;
+            } else {
+                articleTag.forEach(item => {
+                    setDisplayFlex(item);
+                })
+                filterValues(articleTag, btn)
+            }
+        })
+    })
+}
+
+/*
+    Takes an item array as a parameter, and
+    set's the div item's display to flex
+*/
+function resetFilter(itemArr) {
+    itemArr.forEach(item => {
+        setDisplayFlex(item);
+    })
+}
+
+/*
+    Takes an item array and button data as parameters
+    Builds a new array of the items attributes and filters
+    according to button clicked attribute. Next,
+    A new filtered array is made and if the filtered array
+    matches the button clicked then display those items.
+*/
+function filterValues(itemArr, btnData) {
+    let attributeArray = [];
+    let btnAttribute = btnData.getAttribute('data-filter');
+
+    // creates an array of attributes
+    for (let index = 0; index <= itemArr.length - 1; index++) {
+        attributeArray.push(itemArr[index].getAttribute('data-filter'));
+    }
+
+    // creates new result filtered array according to pushed button
+    const result = attributeArray.filter((value => {
+        return value.trim().includes(btnAttribute);
+    }))
+
+    articleTag.forEach(item => {
+        let itemAttribute = item.getAttribute('data-filter');
+        if (result.includes(itemAttribute) && result.length >= 1) {
+            setDisplayFlex(item);
+        } else {
+            setDisplayNone(item);
+        }
+    })
+}
+
+// Set's an item's display as flex
+function setDisplayFlex(item) {
+    item.style.display = 'flex';
+}
+
+// Sets an item's display as none
+function setDisplayNone(item) {
+    item.style.display = 'none';
+}
 
 // run on load
 changeFilterBtnColor();
+applyFilter();
